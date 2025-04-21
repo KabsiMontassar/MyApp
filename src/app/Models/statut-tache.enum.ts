7d31f0a8ce521f4d22ba98d523b4e7e7c3a9f0b9
Kebsi Montassar <114872623+KabsiMontassar@users.noreply.github.com>
2025-04-21 18:10:35 +0100

    Revert "employee&planning"

--- src/app/Models/statut-tache.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum StatutTache {
-  A_FAIRE = 'A_FAIRE',
-  EN_ATTENTE = 'EN_ATTENTE',
-  EN_COURS = 'EN_COURS',
-  TERMINEE = 'TERMINEE',
-  ANNULEE = 'ANNULEE',
-  RETARDEE = 'RETARDEE'
-}
