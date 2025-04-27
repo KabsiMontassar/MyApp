128dfddba65946e55ff2c44c9122bfa43738382f
zayatihamza <125798481+zayatihamza@users.noreply.github.com>
2025-04-27 23:03:15 +0100

    27/04

--- src/app/front-office/pages/my-orders/my-orders.component.ts
+++ src/app/front-office/pages/my-orders/my-orders.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { OrderService } from 'src/app/services/order.service';
+import { Order, OrderProduct } from 'src/app/Models/Order.Model';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-my-orders',
@@ -7,27 +10,45 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./my-orders.component.css']
 })
 export class MyOrdersComponent implements OnInit {
-  orders: any[] = [];
-  statuses = ['PENDING', 'PAID', 'SHIPPED', 'CANCELLED']; // adapt to your statuses
+  
+  statuses : string[]=[]; // Adapt to your app statuses
   selectedStatus: string = '';
+  orders: Order[] = [];
+  orderproducts!: OrderProduct[];
+  private modal: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private orderService: OrderService) {}
 
   ngOnInit(): void {
     this.loadOrders();
+    this.statuses = ['PENDING', 'DELIVERED', 'SHIPPED', 'CANCELLED']; // Adapt to your app statuses
+
   }
 
-  loadOrders() {
-    let url = 'http://localhost:8081/commande/my-orders';
-    if (this.selectedStatus) {
-      url += `?status=${this.selectedStatus}`;
-    }
-    this.http.get<any[]>(url).subscribe(data => {
-      this.orders = data;
+  loadOrders(): void {
+    this.orderService.getAllOrders().subscribe(orders => {
+      this.orders = orders;
     });
   }
 
-  onStatusChange() {
-    this.loadOrders();
+  openModal(order: Order): void {
+    this.orderproducts = order.orderProducts;
+    this.modal = new bootstrap.Modal(document.getElementById('detailsModal'));
+    this.modal.show();
+  }
+
+  closeModal(): void {
+    if (this.modal) {
+      this.modal.hide();
+    }
+  }
+
+  filteredOrders(): Order[] {
+    console.log("I selected" + this.selectedStatus);
+    console.log(this.orders.filter(order => order.status === this.selectedStatus));
+    if (!this.selectedStatus) {
+      return this.orders;
+    }
+    return this.orders.filter(order => order.status === this.selectedStatus);
   }
 }
